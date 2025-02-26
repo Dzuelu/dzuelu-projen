5312d892af2d0752a72e0ab9763ab0d53e8d4f9e
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-02-26 12:18:18 -0500

    remove default publish to npm

--- src/dzueluTypeScriptProject.ts
+++ src/dzueluTypeScriptProject.ts
@@ -25,7 +25,6 @@ export class DzueluTypeScriptProject extends TypeScriptProject {
       prettier: false,
       projenrcTs: true,
       pullRequestTemplate: false,
-      releaseToNpm: true,
       tsconfig: {
         compilerOptions: {
           baseUrl: 'src',
