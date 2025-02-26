aed2089fbb9c5fe43bb43f47e1b435247b406f86
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-02-26 14:06:17 -0500

    releaseToNpm for this project

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,8 @@ import { DzueluTypeScriptProject } from './src/dzueluTypeScriptProject';
 
 const project = new DzueluTypeScriptProject({
   description: 'A simple standard way to setup a repo for typescript in a style Dzuelu likes.',
-  name: 'dzuelu-projen'
+  name: 'dzuelu-projen',
+  releaseToNpm: true
 });
 
 project.addDeps('projen');
