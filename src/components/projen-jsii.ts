d4b20f28d1b3d3c778279f09dbd9f71ed61c9468
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-02-26 19:02:31 -0500

    more progress

--- src/components/projen-jsii.ts
+++ src/components/projen-jsii.ts
@@ -15,17 +15,23 @@ export class ProjenJsii extends Component {
       description: 'Packages artifacts for all target languages'
     });
     this.packageJsTask = this.addPackagingTask('js');
+
+    project.packageTask.reset();
+    project.packageTask.spawn(this.packageJsTask, {
+      // Only run in CI
+      condition: `node -e "if (!process.env.CI) process.exit(1)"`
+    });
+    project.packageTask.spawn(this.packageAllTask, {
+      // Don't run in CI
+      condition: `node -e "if (process.env.CI) process.exit(1)"`
+    });
   }
 
   private addPackagingTask(language: JsiiPacmakTarget): Task {
-    const packageTargetTask = this.tasks.addTask(`package:${language}`, {
+    const packageTargetTask = this.project.tasks.addTask(`package:${language}`, {
       description: `Create ${language} language bindings`
     });
     const commandParts = ['jsii-pacmak', '-v'];
-
-    if (this.package.packageManager === NodePackageManager.PNPM) {
-      commandParts.push("--pack-command 'pnpm pack'");
-    }
 
     commandParts.push(`--target ${language}`);
 
