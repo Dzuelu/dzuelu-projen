77c1f0f2caff2cdb6aa570a297584f3c2a276bca
Dzuelu <24203504+Dzuelu@users.noreply.github.com>
2025-02-26 22:12:44 -0500

    remove eslint config from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './components/github-repo-url';
 export * from './components/linter/dzEslint';
-export * from './components/linter/dzEslintConfig';
 export * from './components/projen-jsii';
 export * from './dzueluTypeScriptProject';
